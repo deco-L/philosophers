86db6f57de3e3a58a06168e45d88c7ac71fb99eb
deco-L <scotchwitchbroom@gmail.com>
2023-11-22 13:25:41 +0900

    init

---
+++ philo/includes/philo.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csakamot <csakamot@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/11 13:32:08 by csakamot          #+#    #+#             */
+/*   Updated: 2023/11/22 12:34:41 by csakamot         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
+# include <sys/time.h>
+# include <unistd.h>
+# include <pthread.h>
+
+# define YES 1
+# define NO 0
+#endif
